9e6038d611706f140e2481991520815aaebbe936
noob9527 <2061864@qq.com>
2023-06-04 16:33:08 +0800

    add ghpages

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "xuekuke-demo",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
